8794b2431c28940fa9e3df172e41a148e5b5b558
Axel <axel.j.zuniga@gmail.com>
2025-07-05 15:53:50 -0600

    final

--- apps/bomberos-de-nosara-frontend/src/components/ui/Contacto/PhoneButton.tsx
+++ apps/bomberos-de-nosara-frontend/src/components/ui/Contacto/PhoneButton.tsx
@@ -1,12 +1,16 @@
-import { a } from "framer-motion/dist/types.d-BSoEx4Ea";
+import { motion } from "framer-motion";
 import { FaPhone } from "react-icons/fa";
 
-export default function PhoneButton(){
-    return(
-        <a href="tel: +50687090614"
-        className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-md flex items-center justify-center space-x-3 mx-auto w-fit">
-             <FaPhone/>
-             <span>LLAMAR +506 87090614</span>
-        </a>
-    )
+export default function PhoneButton() {
+  return (
+    <motion.a
+      href="tel:+50687090614"
+      className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-md flex items-center justify-center space-x-3 mx-auto w-fit"
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <FaPhone />
+      <span>LLAMAR +506 87090614</span>
+    </motion.a>
+  );
 }
