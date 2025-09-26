28eabe8534fd8d0202628f0398feae3c10a1d5ea
Axel <axel.j.zuniga@gmail.com>
2025-09-26 02:44:41 -0600

    good

--- apps/bomberos-de-nosara-backend/src/auth/dto/request-password-reset.dto.ts
+++
@@ -1,8 +0,0 @@
-// src/auth/dto/request-password-reset.dto.ts
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class RequestPasswordResetDto {
-  @IsString()
-  @IsNotEmpty()
-  email!: string; 
-}
