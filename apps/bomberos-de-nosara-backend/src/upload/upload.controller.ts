1bcfed0f63cbd749115faaac26d7d4f2af565896
Brenda <brenda.espinoza.matarrita@est.una.ac.cr>
2025-11-11 13:25:51 -0600

    arreglando rutas de imagenes de noticias

--- apps/bomberos-de-nosara-backend/src/upload/upload.controller.ts
+++ apps/bomberos-de-nosara-backend/src/upload/upload.controller.ts
@@ -1,9 +1,4 @@
-import {
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
@@ -11,10 +6,10 @@ export class UploadController {
   @Post('/uploadImage')
   @UseInterceptors(FileInterceptor('file'))
   uploadImage(@UploadedFile() file: Express.Multer.File) {
+  
     return {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       filename: file.filename,
-      url: `http://localhost:3000/uploads/${file.filename}`,
+      url: `/uploads/${file.filename}`,  // Relative path only
     };
   }
 }
